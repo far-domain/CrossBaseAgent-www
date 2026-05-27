4524c3a30b8c594afadc27ed52cde2e271357a79
cxx <928422955@qq.com>
2026-05-27 20:28:27 +0800

    Redesign interactive static site

--- _next/static/R3jobLbYC2oPBkXWKfUXe/_buildManifest.js
+++ _next/static/R3jobLbYC2oPBkXWKfUXe/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/CrossBaseAgent-www//_next/:path+",
-        "destination": "/CrossBaseAgent-www/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
